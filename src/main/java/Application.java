4b6c7e5d4d01b9ce85e1fb686290e93c2b3e6478
ts092000 <ts092000@gmail.com>
2022-05-11 14:38:07 +0700

    Chapter 6: Spring Configuration Using XML

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,20 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
     public static void main(String args[]) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+//        SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
-        System.out.print(service.findAll().get(0).getFisrtName() + " ");
-        System.out.print(service.findAll().get(0).getLastName());
-
-        System.out.println();
-
-        System.out.print(service.findAll().get(1).getFisrtName() + " ");
-        System.out.print(service.findAll().get(1).getLastName());
+        System.out.println(service.findAll().get(0).getFisrtName());
+//        System.out.print(service.findAll().get(0).getLastName());
+//
+//        System.out.println();
+//
+//        System.out.print(service.findAll().get(1).getFisrtName() + " ");
+//        System.out.print(service.findAll().get(1).getLastName());
     }
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -1,17 +1,28 @@
 package com.pluralsight.service;
 
 import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositorylmpl;
 import com.pluralsight.repository.SpeakerRepository;
 
 import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositorylmpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl() {
+
+    }
+
+    public SpeakerServiceImpl(SpeakerRepository repository) {
+        this.repository = repository;
+    }
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setSpeakerRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
