25cd4613183bb11214a541b58e1fad881208ef57
ts092000 <ts092000@gmail.com>
2022-05-11 10:05:46 +0700

    Chapter 4: Spring Configuration Using Java

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,21 @@
 import com.pluralsight.service.SpeakerService;
 import com.pluralsight.service.SpeakerServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Application {
     public static void main(String args[]) {
-        SpeakerService service = new SpeakerServiceImpl();
+        ApplicationContext appContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+//        SpeakerService service = new SpeakerServiceImpl();
+        SpeakerService service = appContext.getBean("speakerService", SpeakerService.class);
 
         System.out.print(service.findAll().get(0).getFisrtName() + " ");
-        System.out.print(service.findAll().get(0).getLastName());
-
-        System.out.println();
-
-        System.out.print(service.findAll().get(1).getFisrtName() + " ");
-        System.out.print(service.findAll().get(1).getLastName());
+//        System.out.print(service.findAll().get(0).getLastName());
+//
+//        System.out.println();
+//
+//        System.out.print(service.findAll().get(1).getFisrtName() + " ");
+//        System.out.print(service.findAll().get(1).getLastName());
     }
 }
--- src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
+++ src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -8,10 +8,18 @@ import java.util.List;
 
 public class SpeakerServiceImpl implements SpeakerService {
 
-    private SpeakerRepository repository = new HibernateSpeakerRepositorylmpl();
+    private SpeakerRepository repository;
+
+    public SpeakerServiceImpl(SpeakerRepository speakerRepository) {
+        repository = speakerRepository;
+    }
 
     @Override
     public List<Speaker> findAll() {
         return repository.findAll();
+    }
+
+    public void setRepository(SpeakerRepository repository) {
+        this.repository = repository;
     }
 }
